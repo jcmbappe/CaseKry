37c4ae253d445a7d37b18fa5e3ba34b175b46144
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2019-01-24 19:50:45 +0100

    [MASTER] Fragment Transition

--- app/src/main/java/com/example/casekry/activities/MainActivity.kt
+++ app/src/main/java/com/example/casekry/activities/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.example.casekry.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.casekry.R
 import com.example.casekry.databinding.ActivityMainBinding
 import com.example.casekry.fragment.serviceList.ServiceListFragment
-import com.example.casekry.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ServiceListFragment.Interaction {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)?.apply {
-            replaceFragment(ServiceListFragment())
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.container, ServiceListFragment())
+            commit()
         }
     }
 
-    private fun replaceFragment(fragment: Fragment): Boolean {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, fragment, fragment.tag)
-                .commit()
+    override fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(
+                R.anim.enter_from_right,
+                R.anim.exit_to_left,
+                R.anim.enter_from_left,
+                R.anim.exit_to_right
+            )
+            addToBackStack(fragment.tag)
 
-            return true
+
+            replace(R.id.container, fragment, fragment.tag)
+            commit()
         }
- }
+    }
+}
--- app/src/main/java/com/example/casekry/fragment/serviceList/ServiceListFragment.kt
+++ app/src/main/java/com/example/casekry/fragment/serviceList/ServiceListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.casekry.fragment.serviceList
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.casekry.adapter.ServiceAdapter
 import com.example.casekry.databinding.FragmentServiceListBinding
+import com.example.casekry.fragment.form.FormFragment
 import com.example.casekry.repositories.ServicesRepository
 import com.example.casekry.viewModels.BaseViewModel
 import org.koin.android.ext.android.inject
@@ -17,6 +19,17 @@ import org.koin.android.ext.android.inject
 class ServiceListFragment : Fragment() {
     private val servicesRepository: ServicesRepository by inject()
     private lateinit var viewModel: ServiceListViewModel
+
+    private var interaction: Interaction? = null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is Interaction) {
+            interaction = context
+        } else {
+            throw Exception("Context must implement ${Interaction::class.java.simpleName}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +52,23 @@ class ServiceListFragment : Fragment() {
             binding.recyclerView.adapter = this
         }
 
+        binding.fab.setOnClickListener {
+            interaction?.replaceFragment(FormFragment())
+        }
+
         viewModel.getServicesList().observe(this, Observer { list ->
             list?.let { adapter.list = it }
         })
 
         return binding.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        interaction = null
+    }
+
+    interface Interaction {
+        fun replaceFragment(fragment: Fragment)
     }
 }
